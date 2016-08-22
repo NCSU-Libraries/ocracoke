37c63831c68fc85df99c5aa51c816fdec3e92da9
Jason Ronallo <jronallo@gmail.com>
2016-08-22 17:26:10 -0400

    more tries

--- lib/ocr_creator.rb
+++ lib/ocr_creator.rb
@@ -33,7 +33,7 @@ class OcrCreator
         response = @http_client.head url
         cache_file = File.join '/access-images/cache-staging/iiif', @identifier, "/full/full/0/default.jpg"
         tries = 0
-        while !File.exist?(cache_file) && tries < 30
+        while !File.exist?(cache_file) && tries < 60
           puts "waiting for head #{@identifier} #{tries}..."
           sleep 0.1
           tries += 1
