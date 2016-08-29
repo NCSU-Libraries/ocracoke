a8e315107300ab376cfb48d6a10a602e09abf4b3
Jason Ronallo <jronallo@gmail.com>
2016-08-29 09:58:02 -0400

    update puts text

--- app/jobs/concatenate_ocr_txt_job.rb
+++ app/jobs/concatenate_ocr_txt_job.rb
@@ -2,19 +2,19 @@ class ConcatenateOcrTxtJob < ApplicationJob
   queue_as :concatenate_txt
 
   def perform(resource, images)
-    puts "ConcatenateOcrJob: #{resource}"
+    puts "ConcatenateOcrTxtJob: #{resource}"
     concatenator = OcrTxtConcatenator.new(resource, images)
     if concatenator.concatenated_ocr_txt_exists? && !ENV['REDO_OCR']
       puts "Concatenated OCR already exists for #{resource}"
     elsif concatenator.preconditions_met?
-      puts "Doing ConcatenateOcrJob: #{resource}"
+      puts "Doing ConcatenateOcrTxtJob: #{resource}"
       concatenator.concatenate
-      puts "Done ConcatenateOcrJob: #{resource}"
+      puts "Done ConcatenateOcrTxtJob: #{resource}"
       # TODO: Ping another service to let it know it is complete
     else
       # Sometimes files haven't been processed or finished writing yet so we
       # just delay this for a time until it can be added back into its queue.
-      puts "ConcatenateOcrJob: Preconditions not met #{resource}"
+      puts "ConcatenateOcrTxtJob: Preconditions not met #{resource}"
       ConcatenateOcrTxtJob.set(wait: 10.minutes).perform_later resource, images
     end
   end
