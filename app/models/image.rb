10a9137b6d5add8da701f36c7629c97017331a05
Jason Ronallo <jronallo@gmail.com>
2016-09-02 16:44:38 -0400

    add new abilities to models

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,29 @@
 class Image < ApplicationRecord
   belongs_to :resource
+
+  def solr_index
+    indexer = OcrIndexer.new(resource: resource.identifier, image: identifier)
+    indexer.index
+  end
+
+  def ocr
+    OcrCreator.new(identifier).process
+  end
+
+  def create_word_boundaries
+    WordBoundariesCreator.new(identifier).create
+  end
+
+  def queue_ocr_job
+    OcrJob.perform_later identifier, resource.identifier
+  end
+
+  def queue_index_job
+    IndexOcrJob.perform_later resource.identifier, identifier
+  end
+
+  def queue_word_boundaries_job
+    WordBoundariesJob.perform_later identifier
+  end
+
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,6 +1,14 @@
 class Resource < ApplicationRecord
   has_many :images
 
+  def create_pdf
+    PdfCreator.new(identifier, image_identifiers).create
+  end
+
+  def concatenate_txt
+    OcrTxtConcatenator.new(identifier, image_identifiers).concatenate
+  end
+
   def queue_pdf_job
     PdfCreatorJob.perform_later identifier, image_identifiers
   end
