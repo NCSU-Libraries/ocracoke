bcf7e6266aaa1437287b932ec39b45c84ca50baf
Jason Ronallo <jronallo@gmail.com>
2016-09-02 14:24:20 -0400

    if the pdf does not exist requeue

--- app/jobs/pdf_creator_job.rb
+++ app/jobs/pdf_creator_job.rb
@@ -9,8 +9,13 @@ class PdfCreatorJob < ApplicationJob
     elsif pc.preconditions_met?
       puts "Doing PdfCreatorJob: #{resource}"
       pc.create
-      puts "Done PdfCreatorJob: #{resource}"
-      # TODO: Ping another service to let it know it is complete
+      if pc.pdf_exists?
+        puts "Done PdfCreatorJob: #{resource}"
+        # TODO: Ping another service to let it know it is complete
+      else
+        puts "Failed PdfCreatorJob #{resource}"
+        PdfCreatorJob.set(wait: 10.minutes).perform_later resource, images, percentage
+      end
     else
       # Sometimes files haven't been processed or finished writing yet so we
       # just delay this for a time until it can be added back into its queue.
