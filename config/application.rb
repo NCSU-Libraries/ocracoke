068897fa15e0a0528ec41204bf0f13bb3e9c4aff
Jason Ronallo <jronallo@gmail.com>
2016-09-02 19:34:00 -0400

    add lib back to autoload paths

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module IiifSearchInside
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.autoload_paths << Rails.root.join('lib')
     config.autoload_paths << Rails.root.join('app/processing_helpers')
     config.autoload_paths << Rails.root.join('app/processors')
 
