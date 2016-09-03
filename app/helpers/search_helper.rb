6103b27d5a673ef9bd5c2398e876ffa4bb65ae74
Jason Ronallo <jronallo@gmail.com>
2016-09-03 17:41:43 -0400

    allow resource to be multivalued to allow for searching inside different levels

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -2,7 +2,7 @@ module SearchHelper
 
   def annotation_url(id, resource, time)
     # FIXME: Can we use something other than a URN here? What's appropriate/correct?
-    "urn:#{resource}:#{id}:annotation#{time}"
+    "urn:#{params[:id]}:#{id}:annotation#{time}"
   end
 
   def manifest_image_api_id(id)
