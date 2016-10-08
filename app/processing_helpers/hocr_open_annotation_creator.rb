e693e651ed7886b2a5a8e24e9317b83924aee3c4
Jason Ronallo <jronallo@gmail.com>
2016-10-08 00:00:59 -0400

    initial work on annotation lists

---
+++ app/processing_helpers/hocr_open_annotation_creator.rb
@@ -0,0 +1,65 @@
+class HocrOpenAnnotationCreator
+
+  def initialize(hocr_path)
+    @hocr = File.open(hocr_path){ |f| Nokogiri::XML(f) }
+    @identifier = File.basename(hocr_path, '.hocr')
+    @first_two = @identifier[0,2]
+  end
+
+  def annotation_list
+    {
+      :"@context" => "http://iiif.io/api/presentation/2/context.json",
+      :"@id" => annotation_list_id,
+      :"@type" => "sc:AnnotationList",
+      resources: resources
+    }
+  end
+
+  def annotation_list_id
+    # TODO: Use annotation_list_template
+    File.join "https://ocr.lib.ncsu.edu/ocr", @first_two, @identifier, @identifier + '-annotation-list.json'
+  end
+
+  def method
+    #code
+  end
+
+  def resources
+    @hocr.xpath(".//*[contains(@class, 'ocr_line')]").map do |line|
+      text = line.text().gsub("\n", ' ').squeeze(' ').strip
+      if !text.empty?
+        title = line['title']
+        title_parts = title.split('; ')
+        xywh = '0,0,0,0'
+        title_parts.each do |title_part|
+          if title_part.include?('bbox')
+            match_data = /bbox\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)/.match title_part
+            x = match_data[1].to_i
+            y = match_data[2].to_i
+            x1 = match_data[3].to_i
+            y1 = match_data[4].to_i
+            w = x1 - x
+            h = y1 - y
+            xywh = "#{x},#{y},#{w},#{h}"
+          end
+        end
+        annotation(text, xywh)
+      end
+    end.compact
+  end
+
+  def annotation(chars, xywh)
+    {
+      :"@type" => "oa:Annotation",
+      motivation: "sc:painting",
+      resource: {
+        :"@type" => "cnt:ContentAsText",
+        format: "text/plain",
+        chars: chars
+      },
+      # TODO: use canvas_url_template
+      on: "http://d.lib.ncsu.edu/collections/canvas/#{@identifier}#xywh=#{xywh}"
+    }
+  end
+
+end
---
+++ lib/tasks/hocr_annotate.rake
@@ -0,0 +1,13 @@
+namespace :ocracoke do
+
+  desc "create open annotations from hocr file"
+  task :hocr_annotation, [:hocr_path, :outfile] => :environment do |t, args|
+    hr = HocrOpenAnnotationCreator.new File.expand_path(args[:hocr_path])
+    annotations = hr.annotation_list
+    puts annotations.to_json
+    # File.open(File.expand_path(args[:outfile]), 'w') do |fh|
+    #   fh.puts annotations.to_json
+    # end
+  end
+
+end
