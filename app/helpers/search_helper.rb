999f5edd29442a52a04067449dd7864032b6a939
Jason Ronallo <jronallo@gmail.com>
2016-10-08 10:17:47 -0400

    namespace urn. still not right but less bad

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -4,7 +4,7 @@ module SearchHelper
 
   def annotation_url(id, resource, time)
     # FIXME: Can we use something other than a URN here? What's appropriate/correct?
-    "urn:#{params[:id]}:#{id}:annotation#{time}"
+    "urn:ocracoke:#{params[:id]}:#{id}:annotation#{time}"
   end
 
 end
