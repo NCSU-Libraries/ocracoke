7182b0ed2638412c4013477cc3b1a49458d362c8
Jason Ronallo <jronallo@gmail.com>
2020-02-01 22:39:25 -0500

    optimize every night

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,6 +18,10 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
+every :day, at: '3 am' do
+  rake 'ocracoke:solr:optimize', output: { error: '~/ocracoke-solr-optimize-error.log', standard: '~/ocracoke-solr-optimize-standard.log'}
+end
+
 every :day, at: '4 am' do
   rake 'ocracoke:solr:build_suggester', output: { error: '~/ocracoke-solr-build_suggester-error.log', standard: '~/ocracoke-solr-build_suggester-standard.log'}
 end
